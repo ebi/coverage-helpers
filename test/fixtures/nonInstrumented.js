32c12de31fcb3bb17d726281c4d442f0565464cd
Tobias Ebnöther <ebi@gorn.ch>
2011-12-22 16:32:38 +0100

    WS converted everything to spaces

--- test/fixtures/nonInstrumented.js
+++ test/fixtures/nonInstrumented.js
@@ -1,38 +1,38 @@
 var foo = bar;
 
 var some = function (foo) {
-	foo++;
-	if (foo > 3) {
-		var bar = foo;
-		foo++;
-	} else {
-		foo += 2;
-	}
-	return foo;
+    foo++;
+    if (foo > 3) {
+        var bar = foo;
+        foo++;
+    } else {
+        foo += 2;
+    }
+    return foo;
 };
 
 function fooFunction(bla) {
-	return bla + 1;
+    return bla + 1;
 }
 
 //TODO: See how we can nicely wrap this
 function nastyReturn(value) {
-	if (0 === value) {
-		return null;
-	}
-	return value;
+    if (0 === value) {
+        return null;
+    }
+    return value;
 }
 
 (function () {
-	var module = true;
+    var module = true;
 })();
 
 var someObject = {
-	method: function (a, b) {
-		if (a >= 0) {
-			return a + b;
-		} else {
-			return a - b;
-		}
-	}
+    method: function (a, b) {
+        if (a >= 0) {
+            return a + b;
+        } else {
+            return a - b;
+        }
+    }
 };
